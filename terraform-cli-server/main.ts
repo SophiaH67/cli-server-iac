deea9b30423c6ebe6c3223d8f1834840f639e336
Sophia Hage <sophia@sophiah.gay>
2023-09-13 01:59:08 +0200

    add cli server

--- terraform-cli-server/main.ts
+++ terraform-cli-server/main.ts
@@ -1,11 +1,33 @@
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
+import { DockerProvider } from "@cdktf/provider-docker/lib/provider";
+import { Image } from "@cdktf/provider-docker/lib/image";
+import { Container } from "@cdktf/provider-docker/lib/container";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    // define resources here
+    new DockerProvider(this, "docker", {});
+
+    const dockerImage = new Image(this, "cliServerImage", {
+      name: "ghcr.io/nekoluka/cliserver:main",
+      keepLocally: false,
+    });
+
+    new Container(this, "cliServerContainer", {
+      name: "cliserver",
+      image: dockerImage.name,
+      volumes: [
+        { containerPath: "/config.json", hostPath: `${process.cwd()}/config.json` },
+      ],
+      ports: [
+        {
+          internal: 8080,
+          external: 8080,
+        },
+      ],
+    });
   }
 }
 
